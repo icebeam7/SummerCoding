c833e7ed215055f6ea5fcc0ad4c4584d8ec38710
Luis <tony_articuno@hotmail.com>
2023-07-31 18:09:47 +0200

    Add Project code (Part 2 finished) at start of Part 3

---
+++ Part3-MVVM/RefreshingRecipes/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace RefreshingRecipes;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("materialdesignicons-webfont.ttf", "MaterialDesignIcons");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
