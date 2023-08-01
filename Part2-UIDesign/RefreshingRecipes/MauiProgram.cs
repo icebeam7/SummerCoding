b33ab52d09ffd0ccb7a45c4d2e709e0a2bfbcc53
Luis Bryan Oroxón Ramíre <lanzer2002@hotmail.com>
2023-08-01 00:27:48 -0600

    A new Design for RecipeListView

--- Part2-UIDesign/RefreshingRecipes/MauiProgram.cs
+++ Part2-UIDesign/RefreshingRecipes/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("materialdesignicons-webfont.ttf", "MaterialDesignIcons");
 			});
 
 #if DEBUG
